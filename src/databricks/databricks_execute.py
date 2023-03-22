d9e81fcf8898b83f93d6837ec5aba27c472a0f22
Jannic Holzer <jannic.holzer@quantumblack.com>
2023-03-22 22:10:32 +0000

    Point to dbfs location

--- src/databricks/databricks_execute.py
+++ src/databricks/databricks_execute.py
@@ -5,5 +5,9 @@ from kedro.framework.startup import configure_project
 def main():
     package_name = "pyspark_databricks"
     configure_project(package_name)
-    with KedroSession.create(package_name) as session:
+    with KedroSession.create(package_name, conf_source="/dbfs/dbx/test-conf") as session:
         session.run()
+
+
+if __name__ == "__main__":
+    main()
