2d03ec1174bc2a04bdb4adfa49fb37f4402d5833
Jannic Holzer <jannic.holzer@quantumblack.com>
2023-03-28 13:47:48 +0100

    Modify databricks_execute

--- src/databricks/databricks_execute.py
+++ src/databricks/databricks_execute.py
@@ -3,7 +3,7 @@ from kedro.framework.startup import configure_project
 
 
 def main():
-    package_name = "pyspark_databricks"
+    package_name = "pyspark-databricks"
     configure_project(package_name)
     with KedroSession.create(package_name, conf_source="/dbfs/dbx/test-conf") as session:
         session.run()
